54e49b756cb13d967d466d6a22cc18b94fa17812
Patrick Sullivan and Riley Starnes <patrick@>
2016-01-07 14:21:08 -0800

    pass second test to deal with words with multiple letter occurences

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,7 +2,9 @@ class String
   define_method(:anagram) do |comparator|
     split_word = self.split('')
     split_word.each do |letter|
-      if /#{letter}/.match(comparator) == ""
+      self_count = self.gsub(/[^#{letter}]/, '').length
+      comparator_count = comparator.gsub(/[^#{letter}]/, '').length
+      if self_count != comparator_count
         return false
       end
     end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,4 +5,7 @@ describe('String#anagram') do
   it('return true if anagram') do
     expect("word".anagram("wrdo")).to(eq(true))
   end
+  it('returns false if length of original word does not meets length of anagram') do
+    expect("etem".anagram("met")).to(eq(false))
+  end
 end
